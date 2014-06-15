55b404cf6cc1eb8caa1fb78f8bf6cafa061be658
Dan Kolbman <dankolbman@gmail.com>
2014-06-14 21:51:57 -0400

    Add python3 code and results

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-speed-test
+Speed-Test
 ==========
 
 Testing the performance of various languages.
+
+The procedure:
+1. Generate `1000 particles` in a 3D box of size 1
+2. Move each particle randomly, wrapping it to fit in the box, for `10000 steps`
+3. Write each final position to a file
+
+Bench Marks
+==========
+
+Python 3.4 - 175.24s
+
+Dependencies
+==========
+
+Python 3 - Uses numpy to generate random matrices (more efficient than 
+using random)
---
+++ python/python3.py
@@ -0,0 +1,27 @@
+"""
+Test python 3 performance.
+
+Dan Kolbman 2014
+"""
+
+import numpy.random as nprand
+import math
+
+n     = 1000  # particles
+steps = 10000  # How long to simulate
+
+# Create particles in 3D box of 1x1x1
+pos = nprand.rand(n, 3)
+
+for i in range(steps):  # Run all steps
+  for part in pos:      # Move each particle
+    # Scale it down by 0.1
+    part += 0.2*(nprand.rand(3) - [0.5, 0.5, 0.5])
+    # Apply periodicity
+    part -= [ math.floor(x) for x in part ]
+
+# Write the final positions
+f = open('out.dat', 'w')
+for part in pos:
+  f.write(str(part[0])+' '+str(part[1])+' '+str(part[2])+'\n')
+f.close()
